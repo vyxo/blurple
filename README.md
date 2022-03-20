ba1d7e8484c11b9692a9e3370052c84471e87c73
Vyxo <100800443+vyxo@users.noreply.github.com>
2022-03-20 06:51:54 +0000

    added hex colors

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # blurple
-A node module for discord colours!
+A node module for discord colors!
+
+## Installation
+```
+npm install blurple
+```
+
+## Get Started
+You might want to use these colors with the `discord.js` module on npm. They will make your discord bot look professional!
+
+> `blurple.hex.darker` is the same color as a discord embed!
+
+```js
+const blurple = require('./blurple')
+
+// Current Blurple Hex Code
+console.log(blurple.hex.blurple)
+
+// Aqua Role Hex Color
+console.log(blurple.hex.aqua.light)
+
+// Dark Green Role Hex Color
+console.log(blurple.hex.green.dark)
+
+// Generate :root CSS string for your website using hex codes
+console.log(blurple.css())
+
+// Generate a Map of the hex colors
+console.log(blurple.map())
+```
+
+## Feedback
+You can hop onto the Discord or open an issue in the repository.
+
+## Supporting
+I appreciate GitHub followers and discord server members. Hopefully, I can start a YouTube channel!
---
+++ blurple.js
@@ -0,0 +1,68 @@
+/* Code by Vyxo */
+
+module.exports = {
+  hex: {
+    blurple: '#5865F2',
+    dark: '#36393F',
+    darker: '#2F3136',
+    darkest: '#202225',
+    black: '#000000',
+    aqua: {
+      light: '#1ABC9C',
+      dark: '#11806A',
+    },
+    green: {
+      light: '#2ECC71',
+      dark: '#1F8B4C',
+    },
+    blue: {
+      light: '#3498DB',
+      dark: '#206694',
+    },
+    purple: {
+      light: '#9B59B6',
+      dark: '#71368A',
+    },
+    pink: {
+      light: '#E91E63',
+      dark: '#AD1457',
+    },
+    gold: {
+      light: '#F1C40F',
+      dark: '#C27C0E',
+    },
+    orange: {
+      light: '#E67E22',
+      dark: '#A84300',
+    },
+    red: {
+      light: '#E74C3C',
+      dark: '#992D22',
+    },
+  },
+  css: () => {
+    let root = ':root {\n'
+    for (const [key, value] of Object.entries(module.exports.hex)) {
+      if ((typeof value) === 'string') {
+        root += `  --${key}: ${value};\n`
+      } else {
+        root += `  --${key}-light: ${value.light};\n`
+        root += `  --${key}-dark: ${value.dark};\n`
+      }
+    }
+    root += '}'
+    return root
+  },
+  map: () => {
+    let colors = new Map()
+    for (const [key, value] of Object.entries(module.exports.hex)) {
+      if ((typeof value) === 'string') {
+        colors.set(`${key}`, value)
+      } else {
+        colors.set(`${key}_light`, value.light)
+        colors.set(`${key}_dark`, value.dark)
+      }
+    }
+    return colors
+  }
+}
